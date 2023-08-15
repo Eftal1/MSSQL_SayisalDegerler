9c6774707df1425c4404b52ca2f9a7e885b00542
Eftal <101619461+Eftal1@users.noreply.github.com>
2023-08-15 10:10:16 +0300

    Add files via upload

---
+++ Tsql_Sayisal_Degerler.sql
@@ -0,0 +1,29 @@
+
+
+SELECT 3 + 2
+SELECT 3 * 3
+SELECT 4 / 2
+SELECT 9 - 7
+
+--PI : Pi Sayýsýný Verir
+
+SELECT PI()
+
+--SIN : Sinüs alýr.
+
+SELECT SIN(PI())
+
+-- POWER : Üs alýr,
+
+SELECT POWER (2,3)
+
+--ABS : Mutlak Deðer alýr,
+
+SELECT ABS(-12)
+
+-- RAND: 0-1 arasýnda rastgele bir sayý üretir 
+SELECT RAND ()
+
+-- FLOOR : Yuvarlama Yapar
+
+SELECT FLOOR(RAND()*100) 
